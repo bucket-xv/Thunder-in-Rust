359f7b2b66ab3bee4971422d06a5f6e865250f1c
Yuetian Wu <yuetian-wu@outlook.com>
2024-05-02 15:59:46 +0800

    implement level picking

--- src/level_splash.rs
+++ src/level_splash.rs
@@ -1,32 +1,35 @@
-//! This is the page that appears when the game is first launched.
-//! It will display the Thunder game logo for 1 second before transitioning to the main menu.
+//! This is the page that appears after the level is selected and before the corresponding game scene will
+//! start, which will display the selected level to the user again.
+//! It will display the level for 1 second before transitioning to the main menu.
 use bevy::prelude::*;
 
-use super::{despawn_screen, GameState};
+use super::{despawn_screen, GameState, Level, TEXT_COLOR};
 
-// This plugin will display a splash screen with Thunder logo for 1 second before switching to the menu
-pub fn splash_plugin(app: &mut App) {
+// This plugin will display a splash screen with the level information for 1 second before switching to the menu
+pub fn level_splash_plugin(app: &mut App) {
     // As this plugin is managing the splash screen, it will focus on the state `GameState::Splash`
     app
         // When entering the state, spawn everything needed for this screen
-        .add_systems(OnEnter(GameState::Splash), splash_setup)
+        .add_systems(OnEnter(GameState::LevelSplash), level_splash_setup)
         // While in this state, run the `countdown` system
-        .add_systems(Update, countdown.run_if(in_state(GameState::Splash)))
+        .add_systems(Update, countdown.run_if(in_state(GameState::LevelSplash)))
         // When exiting the state, despawn everything that was spawned for this screen
-        .add_systems(OnExit(GameState::Splash), despawn_screen::<OnSplashScreen>);
+        .add_systems(
+            OnExit(GameState::Splash),
+            despawn_screen::<OnLevelSplashScreen>,
+        );
 }
 
 // Tag component used to tag entities added on the splash screen
 #[derive(Component)]
-struct OnSplashScreen;
+struct OnLevelSplashScreen;
 
 // Newtype to use a `Timer` for this screen as a resource
 #[derive(Resource, Deref, DerefMut)]
 struct SplashTimer(Timer);
 
-fn splash_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let icon = asset_server.load("branding/logo.png");
-    // Display the logo
+fn level_splash_setup(mut commands: Commands, level: Res<Level>) {
+    // Display the level information
     commands
         .spawn((
             NodeBundle {
@@ -39,18 +42,23 @@ fn splash_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                 },
                 ..default()
             },
-            OnSplashScreen,
+            OnLevelSplashScreen,
         ))
         .with_children(|parent| {
-            parent.spawn(ImageBundle {
-                style: Style {
-                    // This will set the logo to be 200px wide, and auto adjust its height
-                    width: Val::Px(500.0),
+            parent.spawn(
+                TextBundle::from_section(
+                    format!("Level {}", level.0),
+                    TextStyle {
+                        font_size: 100.0,
+                        color: TEXT_COLOR,
+                        ..default()
+                    },
+                )
+                .with_style(Style {
+                    margin: UiRect::all(Val::Px(50.0)),
                     ..default()
-                },
-                image: UiImage::new(icon),
-                ..default()
-            });
+                }),
+            );
         });
     // Insert the timer as a resource
     commands.insert_resource(SplashTimer(Timer::from_seconds(1.0, TimerMode::Once)));
@@ -63,6 +71,6 @@ fn countdown(
     mut timer: ResMut<SplashTimer>,
 ) {
     if timer.tick(time.delta()).finished() {
-        game_state.set(GameState::Menu);
+        game_state.set(GameState::Game);
     }
 }
